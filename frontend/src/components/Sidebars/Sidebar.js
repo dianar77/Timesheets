b4eddc9ef4ec3a4cbc0ce5823a4a4069cc8e2563
dianar77 <diana_gr_hax@outlook.com>
2024-10-18 17:30:20 -0300

    Changes to sidebar

--- frontend/src/components/Sidebars/Sidebar.js
+++ frontend/src/components/Sidebars/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
 import { 
@@ -17,20 +17,26 @@ import VesselSidebar from './VesselSidebar';
 import WorkOrderSidebar from './WorkOrderSidebar';
 import TimesheetSidebar from './TimesheetSidebar';
 
-function Sidebar({ onDisciplineSelect, onStaffSelect, onClientSelect, onProjectSelect,onVesselSelect,onWorkOrderSelect, onTimesheetSelect }) {
+function Sidebar({ onDisciplineSelect, onStaffSelect, onClientSelect, onProjectSelect, onVesselSelect, onWorkOrderSelect, onTimesheetSelect }) {
+  const [expandedItem, setExpandedItem] = useState(null);
+
+  const handleExpand = (item) => {
+    setExpandedItem(expandedItem === item ? null : item);
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar-header">
         <h2>Dashboard</h2>
       </div>
       <Menu mode="inline" theme="dark">
-        <VesselSidebar onVesselSelect={onVesselSelect} />
-        <WorkOrderSidebar onWorkOrderSelect={onWorkOrderSelect} />
-        <TimesheetSidebar onTimesheetSelect={onTimesheetSelect} />
-        <ProjectSidebar onProjectSelect={onProjectSelect} />
-        <ClientSidebar onClientSelect={onClientSelect} />
-        <DisciplineSidebar onDisciplineSelect={onDisciplineSelect} />
-        <StaffSidebar onStaffSelect={onStaffSelect} />
+        <VesselSidebar onVesselSelect={onVesselSelect} isExpanded={expandedItem === 'vessel'} onExpand={() => handleExpand('vessel')} />
+        <WorkOrderSidebar onWorkOrderSelect={onWorkOrderSelect} isExpanded={expandedItem === 'workOrder'} onExpand={() => handleExpand('workOrder')} />
+        <TimesheetSidebar onTimesheetSelect={onTimesheetSelect} isExpanded={expandedItem === 'timesheet'} onExpand={() => handleExpand('timesheet')} />
+        <ProjectSidebar onProjectSelect={onProjectSelect} isExpanded={expandedItem === 'project'} onExpand={() => handleExpand('project')} />
+        <ClientSidebar onClientSelect={onClientSelect} isExpanded={expandedItem === 'client'} onExpand={() => handleExpand('client')} />
+        <DisciplineSidebar onDisciplineSelect={onDisciplineSelect} isExpanded={expandedItem === 'discipline'} onExpand={() => handleExpand('discipline')} />
+        <StaffSidebar onStaffSelect={onStaffSelect} isExpanded={expandedItem === 'staff'} onExpand={() => handleExpand('staff')} />
       </Menu>
     </div>
   );
